87ce13e4a8edcbd72cc1d5553bdfc2c3125a70d1
Niklas Solomon <niklassolomon@gmail.com>
2022-08-16 17:47:23 -0700

    Filled out Employee class

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require('inquirer');
+
+// Create an empty array list to store employee objects
+const teamData = [];
+
+// Gather manager data
+const addManager = () => {
+    
+}
+    // inquirer.prompt()
+
+    // THEN Build a manager object
+
+// Gather Engineer data
+
+    // inquirer.prompt()
+
+    // THEN Build an engineer object
+
+// Gather intern data
+
+    // inquirer.prompt()
+
+    // THEN Build an intern object
+
+// Ask for which team member they want to add, or are they done
+
+    // inquirer.prompt()
+
+    // THEN deciding which function to call
+
+// Generate the HTML and write it to ad file
