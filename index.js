adde02f273a9b6459c522cdbea6cdc3aa3be2636
Niklas Solomon <niklassolomon@gmail.com>
2022-08-21 15:46:33 -0700

    Added manager prompts to index

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Require employee data
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 // Create an empty array list to store employee objects
 const teamData = [];
 
 // Gather manager data
 const addManager = () => {
-    
-}
     // inquirer.prompt()
+    return inquirer.prompt ([
+        // THEN Build a manager object
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the manager?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?"
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office number?"
+        }
+    ])
+}
 
-    // THEN Build a manager object
 
 // Gather Engineer data
 
