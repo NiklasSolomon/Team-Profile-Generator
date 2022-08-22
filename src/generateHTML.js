3e8f955f958addc2a873e67d4f3d377a349488d8
Niklas Solomon <niklassolomon@gmail.com>
2022-08-21 21:04:51 -0700

    HTML boilerplate added to generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -33,7 +33,7 @@ const engineerEl = function (engineer) {
             </div>
         </div>
     </div>
-    `
+    `;
 };
 
 //Generate element for intern information
@@ -50,7 +50,32 @@ const internEl = function (intern) {
                 <p>Email:<a>${intern.email}</a></p>
                 <p>School: ${intern.school}</p>
             </div>
+        </div>
     </div>
-</div>
-    `
+    `;
+};
+
+const generateWebpage = function (employeeEls) {
+    return`
+        <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <nav>My Team</nav>
+        </header>
+        <main>
+            <div>
+                <div>${employeeEls}</div>
+            </div>
+        </main>
+    </body>
+    </html>
+    `;
 };
